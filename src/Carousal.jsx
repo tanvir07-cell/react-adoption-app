e7a93e82a64b558724f41b8a1d911b9d26657356
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-12-09 05:37:57 +0600

    Chore: for the carousal class component

---
+++ src/Carousal.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Carousal extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  //   function component er body er moto holo ei render() method;
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousal;
